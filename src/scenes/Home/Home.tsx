992fde969979f869cf61cf0bdcae9e3975feb4fa
shindigira <shindigira@gmail.com>
2023-03-01 17:33:54 -0500

    Home Page Implemented * feat: Added Home component * chore: Set transition-transform as the animation type for the Navbar

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -2,36 +2,79 @@ import { useAppSelector, useAppDispatch } from "src/hooks/state";
 import { determineAboveMediumScreens } from "src/hooks/useMediaQuery";
 import { setSelectedPage } from "src/navbarSlice";
 import { SelectedPage } from "src/utility/types";
+
+import { ActionButton } from "src/shared";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
 import HomePageText from "src/assets/HomePageText.png";
+import HomePageGraphic from "src/assets/HomePageGraphic.png";
+import SponsorForbes from "src/assets/SponsorForbes.png";
+import SponsorFortune from "src/assets/SponsorFortune.png";
+import SponsorRedBull from "src/assets/SponsorRedBull.png";
 
 type Props = {};
 
 const Home = (props: Props) => {
   const dispatch = useAppDispatch();
   const isAboveMediumScreens = determineAboveMediumScreens();
+  // const selectPage = (page: SelectedPage) => dispatch(setSelectedPage(page));
+  const navigateContactUs = () =>
+    dispatch(setSelectedPage(SelectedPage.ContactUs));
 
   return (
-    <section id="home">
-      <div className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-        {/* IMAGE AND MAIN HEADER */}
-        <div>
-          {/*  MAIN HEADER */}
-          <div>
-            {/* HEADINGS */}
-            <div>
-              <div>
-                <div>
-                  <img src={HomePageText} alt="home-page-text" />
-                </div>
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-16 md:mt-32 md:basis-3/5">
+          {/* HEADINGS */}
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
               </div>
-
-              <p>Unrivaled</p>
             </div>
+
+            <p className="mt-8 text-sm">
+              Unrivaled gym. Unparalleled training fitness classes. Ideal
+              environment to improve your aesthetics... Get your dream body now!
+            </p>
           </div>
-          {/* FOREGROUND IMAGE */}
-          <div></div>
+
+          {/* ACTIONS */}
+          <div className="mt-8 flex items-center gap-8">
+            <ActionButton>Join Now</ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={navigateContactUs}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16"
+        >
+          <img className="" alt="home-pageGraphic" src={HomePageGraphic} />
         </div>
       </div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[115px] bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
+              <img alt="forbes-sponsor" src={SponsorForbes} />
+              <img alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- src/utility/commonStyles.ts
+++ src/utility/commonStyles.ts
@@ -1,4 +1,4 @@
 export const flexBetween = " flex items-center justify-between ";
 export const menuHidden =
-  " translate-x-[100%] transition duration-500 ease-in-out ";
+  " translate-x-[100%] transition-transform duration-500 ease-in-out ";
 export const menuShow = "translate-x-0";
